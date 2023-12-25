5d658bbc4f4cb03f6dbb5f47c270dc5ec7142ff3
Habibur Rahman <hrsshainik@gmail.com>
2023-12-25 15:10:33 +0600

    Initial Commit

---
+++ hackerNews4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hackerNews4
+//
+//  Created by shopnil hasan on 25/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hackerNews4/hackerNews4App.swift
@@ -0,0 +1,17 @@
+//
+//  hackerNews4App.swift
+//  hackerNews4
+//
+//  Created by shopnil hasan on 25/12/23.
+//
+
+import SwiftUI
+
+@main
+struct hackerNews4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
